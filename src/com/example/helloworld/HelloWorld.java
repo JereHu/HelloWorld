72b9bbfeed3a2debe628fde15af2f4eb64100d4b
JereHu <34283813+JereHu@users.noreply.github.com>
2019-10-07 17:02:12 +0200

    How to Push and commit Hello World

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        String x = "HelloWorld";
+        int MyNum;
+        MyNum = 20039;
+        boolean c = true;
+
+        System.out.printf(x, MyNum, c);
+
+
+    }
+}
